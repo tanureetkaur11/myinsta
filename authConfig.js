b259e267f0f391ea2bb133a7c1db7355e3e0cf04
Tanureet <tanureetkaur11@outlook.com>
2023-04-14 09:17:48 +0100

    Added the config of active directory

--- authConfig.js
+++ authConfig.js
@@ -1,8 +1,8 @@
 const msalConfig = {
     auth: {
-        clientId: "YOUR_APP_CLIENT_ID",
-        authority: "https://YOUR_B2C_TENANT.b2clogin.com/YOUR_B2C_TENANT.onmicrosoft.com/YOUR_SIGN_IN_POLICY",
-        knownAuthorities: ["YOUR_B2C_TENANT.b2clogin.com"],
+        clientId: "e8ae9bb8-e0db-4540-bd24-cdd0a7b3df64",
+        authority: "https://myinstaa.b2clogin.com/myinstaa.onmicrosoft.com/B2C_1_signupsignin",
+        knownAuthorities: ["myinstaa.b2clogin.com"],
     },
     cache: {
         cacheLocation: "localStorage",
