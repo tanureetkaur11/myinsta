801f8eeed998ad506cad743aee685be45fb39e0d
Tanureet <tanureetkaur11@outlook.com>
2023-04-14 10:04:11 +0100

    changed

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 // Sign in
+// Sign in
 function signIn() {
     const loginRequest = {
-        scopes: ["openid", "profile"],
+      scopes: ["openid", "profile"],
     };
-
+  
     myMSALObj
-        .loginPopup(loginRequest)
-        .then((response) => {
-            console.log(response);
-            const account = myMSALObj.getAccount();
-            handleUserState(account);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-}
+      .loginPopup(loginRequest)
+      .then((response) => {
+        console.log(response);
+        const account = myMSALObj.getAccount();
+        handleUserState(account);
+        // Redirect to instahome.html after successful sign-in
+        window.location.href = "instahome.html";
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  
+  function handleUserState(account) {
+    if (account) {
+      // Handle the case when the user is logged in
+      console.log("User is logged in:", account);
+    } else {
+      // Handle the case when the user is not logged in
+      console.log("User is not logged in.");
+    }
+  }
 
 // Sign out
 function signOut() {
     myMSALObj.logout();
 }
 
-// Handle user state
-function handleUserState(account) {
-    if (account) {
-        // User is logged in
-        console.log(account);
-    } else {
-        // User is logged out
-    }
-}
+
 
 myMSALObj.handleRedirectPromise().then((response) => {
     const account = myMSALObj.getAccount();
