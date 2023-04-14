b022be7a1921ffd42786edc971b7202cb97f784c
Tanureet <tanureetkaur11@outlook.com>
2023-04-14 10:06:07 +0100

    revert

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ function signOut() {
     myMSALObj.logout();
 }
 
-
+// Handle user state
+function handleUserState(account) {
+    if (account) {
+        // User is logged in
+        console.log(account);
+    } else {
+        // User is logged out
+    }
+}
 
 myMSALObj.handleRedirectPromise().then((response) => {
     const account = myMSALObj.getAccount();
