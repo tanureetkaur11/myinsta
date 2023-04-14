fbc58ececd307166b58693d910513a77fbe99994
Tanureet <tanureetkaur11@outlook.com>
2023-04-14 09:36:08 +0100

    changed

--- authConfig.js
+++ authConfig.js
@@ -3,6 +3,7 @@ const msalConfig = {
         clientId: "e8ae9bb8-e0db-4540-bd24-cdd0a7b3df64",
         authority: "https://myinstaa.b2clogin.com/myinstaa.onmicrosoft.com/B2C_1_signupsignin",
         knownAuthorities: ["myinstaa.b2clogin.com"],
+        redirectUri: "http://localhost:5500",
     },
     cache: {
         cacheLocation: "localStorage",
